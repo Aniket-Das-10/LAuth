e2691001da5dfd5054bffde730c6eac8692be686
Aniket <aniketdas810144@gmail.com>
2026-03-25 00:14:10 +0530

    create get me

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -14,7 +14,9 @@ exports.register = async (req, res) => {
         }
         const hashpassword = crypto.createHash("sha256").update(password).digest("hex");
         const newUser = await user.create({ username, email, password: hashpassword });
-        const token = jwt.sign({ id: newUser._id }, config.JWT_SECRET, { expiresIn: "1d" });
+        const refreshToken = jwt.sign({ id: newUser._id }, config.JWT_SECRET, { expiresIn: "7d" });
+        res.cookie("refreshToken",refreshToken,{httpOnly:true,secure:true,sameSite:"strict",maxAge:7*24*60*60*1000});
+        const accessToken = jwt.sign({ id: newUser._id }, config.JWT_SECRET, { expiresIn: "15m" });
         res.status(201).json({ 
             message: "User registered successfully",
             user: newUser,
@@ -29,3 +31,30 @@ exports.register = async (req, res) => {
 
 };
 
+exports.getme = async (req, res) => {
+    try {
+        const token  = req.headers.authorization?.split(" ")[1];
+        if(!token){
+            return res.status(401).json({ message: "token not found" });
+        }
+        const decoded = jwt.verify(token,config.JWT_SECRET);
+        console.log("Decoded Token:", decoded);
+        res.status(200).json({ decoded });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+exports.refreshToken = async (req,res) => {
+    try{
+        const refreshToken = req.cookies.refreshToken;
+        if(!refreshToken){
+            return res.status(401).json({ message: "Refresh Token not found"})
+        }
+        const decoded = jwt.verify(refreshToken,config.JWT_SECRET);
+        const accessToken = jwt.sign({id:decoded.id},config.JWT_SECRET,{expiresIn:"15m"});
+        res.status(200).json({ message: "Refresh Token successful",accessToken });
+    }catch(error){
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controller/auth.controller');
+const { register,getme,refreshToken } = require('../controller/auth.controller');
 /**
  * @route POST /api/auth/register
  */
 router.post('/register', register);
+/**
+ * @route GET /api/auth/getme       
+ */
+router.get('/getme', getme);    
+/**
+ * @route GET /api/auth/refresh
+ */
+router.get('/refresh',refreshToken);
 
 module.exports = router;
